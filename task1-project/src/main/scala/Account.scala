7bc85b34a6d83e06a1d54f5d4cede6735f8aa797
kahole <kristian@hole.priv.no>
2017-10-26 22:45:03 +0200

    Task 1 pretty much done, begun task 2

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -4,23 +4,20 @@ class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
 
   private var balance = initialBalance
   
-  def withdraw(amount: Double): Unit = {
+  def withdraw(amount: Double): Unit = synchronized {
+    this.balance = this.balance - amount match {
+      case b if b < 0.0 => throw new NoSufficientFundsException("Not enough money in account")
+      case b if b > this.balance => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
+  }
 
-    this.synchronized {
-      this.balance = this.balance - amount match {
-        case b if b < 0.0 => throw new NoSufficientFundsException("Not enough money in account")
-        case b if b > this.balance => throw new IllegalAmountException("Negative withdrawal amount")
-        case b => b
-      }
+  def deposit(amount: Double): Unit = synchronized {
+    this.balance = this.balance + amount match {
+      case b if b < this.balance => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
     }
   }
-  def deposit(amount: Double): Unit = {
-    this.synchronized {
-      this.balance = this.balance + amount match {
-        case b if b < this.balance => throw new IllegalAmountException("Negative withdrawal amount")
-        case b => b
-      }
-    }
-  }
+
   def getBalanceAmount: Double = { this.balance }
 }
--- task2-project/src/main/scala/Account.scala
+++ task2-project/src/main/scala/Account.scala
@@ -7,9 +7,22 @@ class Account(val bank: Bank, initialBalance: Double) {
   val balance = new Balance(initialBalance)
   val uid = bank.generateAccountId
 
-  def withdraw(amount: Double): Unit = ??? // Like in part 1
-  def deposit(amount: Double): Unit = ??? // Like in part 1
-  def getBalanceAmount: Double = ??? // Like in part 1
+  def withdraw(amount: Double): Unit = synchronized {
+    this.balance.amount = this.balance.amount - amount match {
+      case b if b < 0.0 => throw new NoSufficientFundsException("Not enough money in account")
+      case b if b > this.balance.amount => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
+  }
+
+  def deposit(amount: Double): Unit = synchronized {
+    this.balance.amount = this.balance.amount + amount match {
+      case b if b < this.balance.amount => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
+  }
+
+  def getBalanceAmount: Double = { this.balance.amount }
 
   def transferTo(account: Account, amount: Double) = {
     bank addTransactionToQueue (this, account, amount)
