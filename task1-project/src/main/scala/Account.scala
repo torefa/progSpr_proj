879bc0f97664c9f99c9b23d23281f515e081c674
kahole <kristian@hole.priv.no>
2017-10-26 21:37:57 +0200

    implemented methods in Account class

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,12 +1,25 @@
+
+import exceptions.NoSufficientFundsException
+import exceptions.IllegalAmountException
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
 
   private var balance = initialBalance
   
   def withdraw(amount: Double): Unit = {
-    this.balance -= amount
+
+    this.balance = this.balance - amount match {
+      case b if b < 0.0 => throw new NoSufficientFundsException("Not enough money in account")
+      case b if b > this.balance => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
   }
   def deposit(amount: Double): Unit = {
-    this.balance += amount
+
+    this.balance = this.balance + amount match {
+      case b if b < this.balance => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
   }
   def getBalanceAmount: Double = { this.balance }
 }
