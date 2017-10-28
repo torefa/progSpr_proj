bcd62ccc0e358d50665b55c98e7b9a45b0212b57
kahole <kristian@hole.priv.no>
2017-10-29 01:26:44 +0200

    T3 all tests successfull

--- task3-project/src/main/scala/Account.scala
+++ task3-project/src/main/scala/Account.scala
@@ -24,21 +24,36 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
   def getTransactions: List[Transaction] = {
     // Should return a list of all Transaction-objects stored in transactions
-    ???
+    transactions.values.toList
   }
 
   def allTransactionsCompleted: Boolean = {
     // Should return whether all Transaction-objects in transactions are completed
-    ???
+    getTransactions.foldLeft(true)(_ && _.isCompleted)
   }
 
-  def withdraw(amount: Double): Unit = ??? // Like in part 2
-  def deposit(amount: Double): Unit = ??? // Like in part 2
-  def getBalanceAmount: Double = ??? // Like in part 2
+  /* TODO: !! jeg fjernet synchronized keywordet fra de tre metodene, fordi jeg tror ikke det trengs.*/
+
+  def withdraw(amount: Double): Unit = synchronized {
+    balance.amount = balance.amount - amount match {
+      case b if b < 0.0 => throw new NoSufficientFundsException("Not enough money in account")
+      case b if b > balance.amount => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
+  }
+
+  def deposit(amount: Double): Unit = synchronized {
+    balance.amount = balance.amount + amount match {
+      case b if b < balance.amount => throw new IllegalAmountException("Negative withdrawal amount")
+      case b => b
+    }
+  }
+
+  def getBalanceAmount: Double = synchronized { balance.amount }
 
   def sendTransactionToBank(t: Transaction): Unit = {
     // Should send a message containing t to the bank of this account
-    ???
+    BankManager.findBank(bankId) ! t
   }
 
   def transferTo(accountNumber: String, amount: Double): Transaction = {
@@ -55,9 +70,7 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
           t.status = TransactionStatus.FAILED
       }
     }
-
     t
-
   }
 
   def reserveTransaction(t: Transaction): Boolean = {
@@ -73,17 +86,42 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     case TransactionRequestReceipt(to, transactionId, transaction) => {
       // Process receipt
-      ???
+      if (transactions.contains(transactionId)) {
+
+        /*Her er det ikke vits å hente t fra mappen, fordi vi sender rundt referanser av Transaksjoner.
+         men kan være greit å late det er pass by value, tror det er meningen,
+         spesielt hvis man tenker på hvordan et bank-system med flere banker egentlig ville vært*/
+
+        val t = transactions(transactionId) //transaction
+        t.status = transaction.status
+        t.receiptReceived = true
+
+        /*If transaction fails, re-instate the amount that was withdrawn earlier.*/
+        if (t.status == TransactionStatus.FAILED) {
+          deposit(t.amount)
+        }
+
+      }
     }
 
-    case BalanceRequest => ??? // Should return current balance
+    case BalanceRequest => sender ! getBalanceAmount // Should return current balance
 
     case t: Transaction => {
       // Handle incoming transaction
-      ???
+      
+      try {
+        deposit(t.amount)
+        t.status = TransactionStatus.SUCCESS
+
+      } catch {
+        case _: IllegalAmountException =>
+          t.status = TransactionStatus.FAILED
+      }
+
+      sender ! TransactionRequestReceipt(t.from, t.id, t)
     }
 
-    case msg => ???
+    case msg => {/*???*/}
   }
 
 
--- task3-project/src/main/scala/Bank.scala
+++ task3-project/src/main/scala/Bank.scala
@@ -41,28 +41,75 @@ class Bank(val bankId: String) extends Actor {
   }
 
   override def receive = {
-    case CreateAccountRequest(initialBalance) => ??? // Create a new account
-    case GetAccountRequest(id) => ??? // Return account
+    case CreateAccountRequest(initialBalance) => sender ! createAccount(initialBalance) // Create a new account
+    case GetAccountRequest(id) => sender ! findAccount(id) // Return account
     case IdentifyActor => sender ! this
     case t: Transaction => processTransaction(t)
 
     case t: TransactionRequestReceipt => {
       // Forward receipt
-      ???
+
+      implicit val timeout = new Timeout(5 seconds)
+      
+      val toBankId = t.toAccountNumber.substring(0, 4)
+      val toAccountId = t.toAccountNumber.substring(4)
+      val isInternal = toBankId == bankId
+
+      if (isInternal) {
+        findAccount(toAccountId) match {
+          case Some(account) => account forward t
+          case None => println("Account doesnt exist.")
+        }
+      } else {
+        findOtherBank(toBankId) match {
+          case Some(bank) => bank forward t
+          case None => println("Bank doesnt exist.")
+        }
+      }
     }
 
-    case msg => ???
+    case msg => {/*???*/}
   }
 
   def processTransaction(t: Transaction): Unit = {
+
     implicit val timeout = new Timeout(5 seconds)
-    val isInternal = t.to.length <= 4
+
+    var isInternal = t.to.length <= 4
     val toBankId = if (isInternal) bankId else t.to.substring(0, 4)
     val toAccountId = if (isInternal) t.to else t.to.substring(4)
     val transactionStatus = t.status
-    
+
+    if (toBankId == bankId) {
+      isInternal = true
+    }
+
     // This method should forward Transaction t to an account or another bank, depending on the "to"-address.
     // HINT: Make use of the variables that have been defined above.
-    ???
+    // if (transactionStatus == TransactionStatus.FAILED) {
+    //   sender ! TransactionRequestReceipt(t.from, t.id, t)
+    // }
+
+    if (isInternal) {
+      findAccount(toAccountId) match {
+        case Some(account) => account forward t
+        case None => {
+          println("Account doesnt exist.")
+          t.status = TransactionStatus.FAILED
+          sender ! TransactionRequestReceipt(t.from, t.id, t)
+        }
+      }
+    } else {
+
+      findOtherBank(toBankId) match {
+        case Some(bank) => bank forward t
+        case None => {
+          println("Bank doesnt exist.")
+          t.status = TransactionStatus.FAILED
+          sender ! TransactionRequestReceipt(t.from, t.id, t)
+        }
+      }
+    }
+    
   }
 }
